828204b81018c6aa119d15b1b22b4f9eaf9ea446
Guillaume Viguier <guillaume@viguierjust.com>
2015-08-24 12:00:25 +0200

    Handling forks and drupal core

--- drupdate.php
+++ drupdate.php
@@ -1,27 +1,73 @@
 <?php
 
+require_once('./conf.php');
 require_once('./lib.php');
+require_once(__DIR__ . '/github-php-client/client/GitHubClient.php');
 
-$url = 'https://github.com/humanitarianresponse/site';
-$branch = 'dev';
+$client = new GitHubClient();
+$client->setCredentials($conf['username'], $conf['password']);
+
+$url = 'git@github.com:guillaumev/gvj.git';
+$branch = 'master';
+
+/**
+ * Forks a repository
+ *
+ * @param $owner
+ *   The owner of the repository to be forked
+ * @param $repo
+ *   The repository to be forked
+ */
+function drupdate_fork($owner, $repo) {
+  global $client;
+  return $client->request("/repos/$owner/$repo/forks", 'POST', $data, 202, 'GitHubRepo');
+}
+
+/**
+ * Clone a repository locally and sync it if it's a fork
+ *
+ * @param $owner
+ *  The owner of the github repository
+ * @param $repo
+ *  The repository to be cloned
+ * @param $branch
+ *  The branch to be cloned
+ */
+function _drupdate_clone($owner, $repo, $branch) {
+  global $client;
+  $cmd = "git clone git@github.com:'.$owner.'/'.$repo.' repository";
+  exec($cmd, $output, $return);
+  if ($return == 0) {
+    // Find out if it's a fork
+    $repo_object = $client->repos->get($owner, $repo);
+    if ($repo_object->getFork()) {
+      // This is a fork, it needs to be synchronized
+      // Get parent repository clone url
+      $parent = $repo_object->getParent();
+      $cmd = 'cd repository;git remote add upstream '.$parent->getCloneUrl().';git fetch upstream; git checkout '.$branch.';git merge upstream/'.$branch;
+      exec($cmd, $output, $return);
+    }
+  }
+  return $return;
+}
+
 
 /**
  * Updates the Drupal modules of a repository
  *
- * @param $url
- *   The URL of the git repository
- * @param $branch
- *   The branch to work on
+ * @param $owner
+ *   The owner of the github repository
+ * @param $repo
+ *   The repository to work on
  * @param $options
  *   Extra options:
  *    - merge: whether to merge back in the main branch after pushing
  *    - ignore: array of modules or themes that should be ignored in the update
  */
-function drupdate($url, $branch, $options = array()) {
+function drupdate($owner, $repo, $branch, $options = array()) {
   $to_update = array();
   // Step 1: clone repository
-  $cmd = "git clone --branch=$branch $url repository";
-  exec($cmd, $output, $return);
+  $return = _drupdate_clone($owner, $repo, $branch);
   if ($return == 0) {
     // Step 2: get list of modules with their version
     $dir_iterator = new RecursiveDirectoryIterator("./repository");
@@ -30,7 +76,7 @@ function drupdate($url, $branch, $options = array()) {
       if ($file->getExtension() == 'info') {
         $data = file_get_contents($file->getRealPath());
         $info = drupal_parse_info_format($data);
-        if (isset($info['project']) && $info['project'] == $file->getBasename('.info') && isset($info['version']) && strpos($info['version'], 'dev') === FALSE) {
+        if (isset($info['project']) && ($info['project'] == $file->getBasename('.info') || $info['project'] == 'drupal') && isset($info['version']) && strpos($info['version'], 'dev') === FALSE) {
           $projects[$info['project']]['info'] = $info;
         }
       }
@@ -47,7 +93,7 @@ function drupdate($url, $branch, $options = array()) {
       if ($xml) {
         $available = update_parse_xml($xml);
         update_calculate_project_update_status(NULL, $project, $available);
-        if (isset($project['existing_version']) && isset($project['recommended']) && $project['existing_version'] != $project['recommended']) {
+        if (isset($project['existing_version']) && isset($project['recommended']) && $project['existing_version'] != $project['recommended'] && (!isset($options['ignore']) || (isset($options['ignore']) && !in_array($name, $options['ignore'])))) {
           $to_update[] = $name;
         }
       }
@@ -55,16 +101,34 @@ function drupdate($url, $branch, $options = array()) {
 
     // Step 4: download updated modules with drush
     if (!empty($to_update)) {
-      $modules = implode(' ', $to_update);
-      $cmd = "cd repository; drush -y dl $modules";
-      exec($cmd, $output, $return);
-      if ($return == 0) {
-        // Step 5: commit the changes in an update branch
-        $date = date('Y-m-d');
-        $cmd = 'cd repository; git checkout -b update-' . $date . '; git add --all .; git commit -am "Updated ' . $modules.'"';
+      // TODO: handle patches
+      // Handle drupal core specifically
+      if (in_array('drupal', $to_update)) {
+        $dkey = array_search('drupal', $to_update);
+        if ($dkey !== FALSE) {
+          unset($to_update[$dkey]);
+          // Update drupal core
+          // Download in another folder
+          $cmd = "drush dl drupal -y --destination=core --drupal-project-rename";
+          exec($cmd, $output, $return);
+          if ($return == 0) {
+            $cmd = "cp -R core/drupal/* repository";
+            exec($cmd, $output, $return);
+          }
+        }
+      }
+      if (!empty($to_update)) {
+        $modules = implode(' ', $to_update);
+        $cmd = "cd repository; drush -y dl $modules";
         exec($cmd, $output, $return);
-        // TODO: push the updated modules to the branch
-        // TODO: merge the update branch back into the main one
+        if ($return == 0) {
+          _drupdate_commit($modules, $options);
+        }
+      }
+      else {
+        // We are just update drupal core
+        $modules = 'drupal';
+        _drupdate_commit($modules, $options);
       }
     }
         
@@ -72,4 +136,29 @@ function drupdate($url, $branch, $options = array()) {
   }
 }
 
-drupdate($url, $branch);
+function _drupdate_commit($modules, $options) {
+  // Step 5: commit the changes in an update branch
+  $date = date('Y-m-d');
+  $cmd = 'cd repository; git checkout -b update-' . $date . '; git add --all .; git commit -am "Updated ' . $modules.'"';
+  exec($cmd, $output, $return);
+  if ($return == 0) {
+    // push the updated modules to the branch
+    $cmd = 'cd repository; git push origin update-' . $date;
+    exec($cmd, $output, $return);
+    // TODO: create the pull request
+    $pr_data = array();
+    $pr_data['title'] = 'Drupal module updates '.$date;
+    $pr_data['head'] = ''; // TODO
+    $pr_data['base'] = ''; // TODO
+    $pr_data['body'] = 'Updated '.$modules;
+    if ($return == 0 && isset($options['merge']) && $options['merge'] == true) {
+      // Merge the update branch back into the main one
+      $cmd = 'git checkout '.$branch.'; git merge update-' . $date.'; git push origin '.$branch;
+      exec($cmd, $output, $return);
+    }
+  }
+}
+
+$options = array('ignore' => array('bootstrap'), 'merge' => false);
+
+drupdate_clone('guillaumev', 'Spoon-Knife', 'master');
