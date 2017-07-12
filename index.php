d15a527133375d9be45ffe2eba7634f334a9d756
Guillaume Viguier <guillaume@viguierjust.com>
2017-07-12 16:42:21 +0200

    #6 - security updates

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ if (!empty($_GET) && $_GET['owner'] && $_GET['repository'] && $_GET['branch']) {
   $output = [];
   // Build command
   $command = 'php /var/www/drupdate/lib/drupdate.php -o '.escapeshellarg($_GET['owner']).' -r '.escapeshellarg($_GET['repository']).' -b '.escapeshellarg($_GET['branch']);
+  if ($_GET['security']) {
+    $command .= ' -s';
+  }
   exec($command, $output);
   echo join("\n", $output);
 }
